22c0e6ce59fb538ea2ec43b6e65df6e0e1ba7d90
Fernando Aceituno <aceituno.fernando@gmail.com>
2023-01-01 23:33:44 -0300

    primer form

--- src/modules/products/uom/UomDashboard.tsx
+++ src/modules/products/uom/UomDashboard.tsx
@@ -1,10 +1,63 @@
-import { Button } from "@chakra-ui/react";
+
+import { useForm } from 'react-hook-form'
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Box,
+  Select
+} from '@chakra-ui/react'
+
+
 
 const UomDashboard = () => {
+        const {
+          handleSubmit,
+          register,
+          formState: { errors, isSubmitting },
+        } = useForm();
+      
+        const onSubmit = (data:object) => console.log(data);
+
     return (
-        <div>uom dashboard
-            <Button colorScheme="purple">Test</Button>
-        </div>
+    <Box display="flex"
+    h="100vh"
+    w="1000px"
+    bg="#191B1F"
+    alignItems="center"
+    justifyContent="center"
+    color= 'tomato'
+    flexDirection='column'
+    > 
+    <h1>ALTA - UNIDAD DE MEDIDA</h1>
+
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl p={'30px'}>
+        <Select
+          id='name'
+          placeholder='Seleccion de unidad de medida'
+          {...register('name', {
+            required: 'This is required'
+           
+          })}
+        >
+                <option> Kilogramo</option>
+                <option> Gramo</option>
+                <option> Cantidad</option>
+                <option> Fraccion</option>
+
+        </Select>
+        <FormErrorMessage>
+         
+        </FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+        Submit
+      </Button>
+    </form>
+    </Box>
     )
 }
 
